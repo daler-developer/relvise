a4c0a40c2ca5ea5f0c04f920215abea36a8915ea
Daler <daler.developer@mail.ru>
2021-09-16 23:07:47 +0500

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const { series, src, dest, watch } = require('gulp')
+const fileInclude = require('gulp-file-include')
+const sass = require('gulp-sass')(require('sass'))
+
+
+const html = (cb) => {
+  return src('./#src/index.html')
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest('./build/'))
+}
+
+const scss = (cb) => {
+  return src('./#src/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./build/'))
+}
+
+const js = (cb) => {
+  return src('./#src/index.js')
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest('./build/'))
+}
+
+const images = (cb) => {
+  return src('./#src/images/**/*')
+    .pipe(dest('./build/images/'))
+}
+
+const watchAll = (cb) => {
+  watch('./#src/**/*.html', html)
+  watch('./#src/**/*.scss', scss)
+  watch('./#src/**/*.js', js)
+  watch('./#src/images/', images)
+}
+
+exports.default = series(html, scss, js, images, watchAll)
